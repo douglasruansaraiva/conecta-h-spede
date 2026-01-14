3334ac37c58792b733b34f4c39046fb48ba31bd0
Douglas Ruan <douglasruansariva1223@gmail.com>
2026-01-14 16:39:33 +0000

    File changes

--- functions/exportCalendar.ts
+++ functions/exportCalendar.ts
@@ -54,56 +54,57 @@ Deno.serve(async (req) => {
       company_id: company_id
     });
 
-    // Gerar arquivo iCal
+    // Gerar arquivo iCal com compatibilidade total VRBO
     let ical = 'BEGIN:VCALENDAR\r\n';
     ical += 'VERSION:2.0\r\n';
-    ical += 'PRODID:-//Conecta Hospede//NONSGML v1.0//EN\r\n';
+    ical += 'PRODID:-//Conecta Hospede//Calendar Export//EN\r\n';
     ical += 'CALSCALE:GREGORIAN\r\n';
     ical += 'METHOD:PUBLISH\r\n';
     ical += `X-WR-CALNAME:${accommodation.name || 'Calendario'}\r\n`;
     ical += 'X-WR-TIMEZONE:America/Sao_Paulo\r\n';
+    ical += 'X-PUBLISHED-TTL:PT15M\r\n';
 
     const now = new Date().toISOString().replace(/[-:]/g, '').split('.')[0] + 'Z';
 
-    // Adicionar reservas como eventos
+    // Adicionar reservas como eventos (formato compatível VRBO)
     reservations.forEach(res => {
       const checkIn = res.check_in.replace(/-/g, '');
       const checkOut = res.check_out.replace(/-/g, '');
-      
+      const uid = `res-${res.id}-${checkIn}@conectahospede.app`;
+
       ical += 'BEGIN:VEVENT\r\n';
-      ical += `UID:res-${res.id}@conectahospede.app\r\n`;
+      ical += `UID:${uid}\r\n`;
       ical += `DTSTAMP:${now}\r\n`;
       ical += `DTSTART;VALUE=DATE:${checkIn}\r\n`;
       ical += `DTEND;VALUE=DATE:${checkOut}\r\n`;
-      ical += `SUMMARY:Reservado${res.guest_name ? ' - ' + res.guest_name : ''}\r\n`;
-      
-      if (res.notes) {
-        ical += `DESCRIPTION:${res.notes.replace(/\n/g, '\\n')}\r\n`;
-      }
-      
+      ical += `SUMMARY:Reservado\r\n`;
+      ical += `DESCRIPTION:Reserva confirmada\r\n`;
       ical += 'STATUS:CONFIRMED\r\n';
       ical += 'TRANSP:OPAQUE\r\n';
+      ical += 'SEQUENCE:0\r\n';
+      ical += `CREATED:${now}\r\n`;
+      ical += `LAST-MODIFIED:${now}\r\n`;
       ical += 'END:VEVENT\r\n';
     });
 
-    // Adicionar datas bloqueadas
+    // Adicionar datas bloqueadas (formato compatível VRBO)
     blockedDates.forEach(block => {
       const start = block.start_date.replace(/-/g, '');
       const end = block.end_date.replace(/-/g, '');
-      
+      const uid = `block-${block.id}-${start}@conectahospede.app`;
+
       ical += 'BEGIN:VEVENT\r\n';
-      ical += `UID:block-${block.id}@conectahospede.app\r\n`;
+      ical += `UID:${uid}\r\n`;
       ical += `DTSTAMP:${now}\r\n`;
       ical += `DTSTART;VALUE=DATE:${start}\r\n`;
       ical += `DTEND;VALUE=DATE:${end}\r\n`;
-      ical += `SUMMARY:Bloqueado${block.reason ? ' - ' + block.reason : ''}\r\n`;
-      
-      if (block.source === 'ical_import') {
-        ical += 'DESCRIPTION:Importado de calendário externo\r\n';
-      }
-      
+      ical += `SUMMARY:Indisponível\r\n`;
+      ical += `DESCRIPTION:Data bloqueada\r\n`;
       ical += 'STATUS:CONFIRMED\r\n';
       ical += 'TRANSP:OPAQUE\r\n';
+      ical += 'SEQUENCE:0\r\n';
+      ical += `CREATED:${now}\r\n`;
+      ical += `LAST-MODIFIED:${now}\r\n`;
       ical += 'END:VEVENT\r\n';
     });
 
@@ -115,9 +116,12 @@ Deno.serve(async (req) => {
         'Content-Type': 'text/calendar; charset=utf-8',
         'Content-Disposition': 'inline; filename="calendar.ics"',
         'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type'
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'X-Content-Type-Options': 'nosniff'
       }
     });
 
